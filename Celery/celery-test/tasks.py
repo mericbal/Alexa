dce4c9f6a44eda59efe9d001d448380c164d7dd1
Meric BAL <MB@Meric-MacBook-Air.local>
2017-02-06 17:36:36 -0800

    Added some codes.

--- Celery/celery-test/tasks.py
+++ Celery/celery-test/tasks.py
@@ -1,11 +1,27 @@
+# celery worker -A tasks -l INFO
+# rabbitmq-server
+
 from celery import Celery
 
 app = Celery()
 
 @app.task
-def hello():
-	return 'LET THERE BE LIGHT'
+def hello(string):
+	return string
 
 @app.task
 def add(x,y):
 	return x + y
+
+@app.task
+def counter(x):
+	while x >= 1:
+		print	"Number is -> %d " % x
+		x-=1
+
+# @app.task
+# def loop():
+# 	x=1
+# 	while True:
+# 		print	"Infinity number %d " % x
+# 		x += 1
